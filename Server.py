64931f8e591e26faa0a861312e7094fbec973a7c
Ubuntu <ubuntu@ip-172-31-3-23.ap-northeast-2.compute.internal>
2017-08-20 22:44:34 +0000

    add sign up work

--- Server.py
+++ Server.py
@@ -3,8 +3,7 @@ from select import select
 import sys
 import json
 
-from SocketReadThread import *
-import SocketWriteThread
+from SocketReadWriteThread import *
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -17,5 +16,5 @@ while(1):
   print("socket wait")
   client_socket, client_address = server_socket.accept() 
   print("socket accepted - " + client_address[0] + ":" + str(client_address[1]) + " is accepted")
-  socket_read_thread = SocketReadThread(client_socket, client_socket_list);
+  socket_read_thread = SocketReadWriteThread(client_socket, client_socket_list);
   socket_read_thread.start()
